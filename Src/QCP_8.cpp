615ff60e172fd8d64ee938d0939c5608456624e0
minjae-jo-phys <m.jo@imperial.ac.uk>
2022-06-20 14:53:15 +0100

    fix(QJMC_QCP): remove a redundant input N

--- Src/QCP_8.cpp
+++ Src/QCP_8.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 using namespace Snu::Cnrc;
-const int NN = 8;
+const int N = 8;
 
 typedef complex<double> dcomplex;
 using Size = unsigned int;
@@ -18,25 +18,24 @@ using Size = unsigned int;
 class QCP_1D {
 private:
     Size number_of_time_step;
-    Size N;
     double omega;
     Size number_of_ensemble;
     double gamma;
     double dt;
 
 public:
-    QCP_1D(const Size, const Size, const double, const Size, const double, const double);
+    QCP_1D(const Size, const double, const Size, const double, const double);
     ~QCP_1D();
     void QJMC();
     vector<dcomplex> normalize(vector<dcomplex>& state);
     vector<dcomplex> vectorCal(vector<dcomplex>& state, vector<dcomplex>& k, const double dt, const double num);
-    double calDecayDeltaP(Size site, const double dt, vector<dcomplex>& state, const Size N, const double gamma);
-    vector<dcomplex> emissionDecay(Size site, vector<dcomplex>& state, const Size N);
-    vector<dcomplex> dcdt(vector<dcomplex> state, const Size N, const double omega, const double gamma);
+    double calDecayDeltaP(Size site, const double dt, vector<dcomplex>& state, const double gamma);
+    vector<dcomplex> emissionDecay(Size site, vector<dcomplex>& state);
+    vector<dcomplex> dcdt(vector<dcomplex> state, const double omega, const double gamma);
 };
 
-QCP_1D::QCP_1D(const Size _number_of_time_step_, const Size _N_, const double _omega_, const Size _number_of_ensemble_, const double _gamma_, const double _dt_)
-: number_of_time_step(_number_of_time_step_), N(_N_), omega(_omega_), number_of_ensemble(_number_of_ensemble_), gamma(_gamma_), dt(_dt_) {
+QCP_1D::QCP_1D(const Size _number_of_time_step_, const double _omega_, const Size _number_of_ensemble_, const double _gamma_, const double _dt_)
+: number_of_time_step(_number_of_time_step_), omega(_omega_), number_of_ensemble(_number_of_ensemble_), gamma(_gamma_), dt(_dt_) {
 }
 
 QCP_1D::~QCP_1D() {
@@ -59,18 +58,18 @@ void QCP_1D::QJMC() {
         state[pow(2,N)-1] = {1, 0};
 
         for(Size t=0; t<number_of_time_step; ++t) {
-            for(Size site=0; site<N; ++site) deltaP[site] = calDecayDeltaP(site, dt, state, N, gamma);
+            for(Size site=0; site<N; ++site) deltaP[site] = calDecayDeltaP(site, dt, state, gamma);
             partial_sum(deltaP.begin(), deltaP.end(), deltaP.begin());
             double p = rnd();
             siteEvolved = distance(deltaP.begin(), lower_bound(deltaP.begin(), deltaP.end(), p));
             if(siteEvolved != N) {
-                state = emissionDecay(siteEvolved, state, N);
+                state = emissionDecay(siteEvolved, state);
             }
             else { // no emission takes place
-                k1 = dcdt(state, N, omega, gamma);
-                k2 = dcdt(vectorCal(state, k1, dt, 2), N, omega, gamma);
-                k3 = dcdt(vectorCal(state, k2, dt, 2), N, omega, gamma);
-                k4 = dcdt(vectorCal(state, k3, dt, 1), N, omega, gamma);
+                k1 = dcdt(state, omega, gamma);
+                k2 = dcdt(vectorCal(state, k1, dt, 2), omega, gamma);
+                k3 = dcdt(vectorCal(state, k2, dt, 2), omega, gamma);
+                k4 = dcdt(vectorCal(state, k3, dt, 1), omega, gamma);
                 for(Size i=0; i<pow(2, N); ++i) {
                     //state[i] = (state[i] + dt/6.*(k1[i]+2.*k2[i]+2.*k3[i]+k4[i])) / (dcomplex)(sqrt(1.-deltaP[N-1]));
                     state[i] = (state[i] + dt/6.*(k1[i]+2.*k2[i]+2.*k3[i]+k4[i]));
@@ -78,7 +77,7 @@ void QCP_1D::QJMC() {
                 state = normalize(state);
             }  
             for(Size i=0; i<pow(2, N); ++i) {
-                orderParameter[t] += (bitset<NN>(i).count()*norm(state[i]));
+                orderParameter[t] += (bitset<N>(i).count()*norm(state[i]));
             }
         }
     }
@@ -99,21 +98,21 @@ vector<dcomplex> QCP_1D::vectorCal(vector<dcomplex>& state, vector<dcomplex>& k,
 }
 
 // Decay Lindblad Operator
-double QCP_1D::calDecayDeltaP(Size site, const double dt, vector<dcomplex>& state, const Size N, const double gamma) {
+double QCP_1D::calDecayDeltaP(Size site, const double dt, vector<dcomplex>& state, const double gamma) {
     double deltaP = 0;
     for(Size i=0; i<pow(2, N); ++i) {
-        if(bitset<NN>(i)[site] == 1) {
+        if(bitset<N>(i)[site] == 1) {
             deltaP += gamma*dt*(norm(state[i]));
         }
     }
     return deltaP;
 }
 
-vector<dcomplex> QCP_1D::emissionDecay(Size site, vector<dcomplex>& state, const Size N) {
+vector<dcomplex> QCP_1D::emissionDecay(Size site, vector<dcomplex>& state) {
     vector<dcomplex> cc(pow(2, N));
     for(Size i=0; i<pow(2, N); ++i) {
-        if(bitset<NN>(i)[site] == 1) {
-            cc[bitset<NN>(i).flip(site).to_ulong()] += state[i];
+        if(bitset<N>(i)[site] == 1) {
+            cc[bitset<N>(i).flip(site).to_ulong()] += state[i];
         }
     }
     cc = normalize(cc);
@@ -121,18 +120,18 @@ vector<dcomplex> QCP_1D::emissionDecay(Size site, vector<dcomplex>& state, const
 }
 
 // Runge-Kutta method for the differential value of wavefunction
-vector<dcomplex> QCP_1D::dcdt(vector<dcomplex> state, const Size N, const double omega, const double gamma) {
+vector<dcomplex> QCP_1D::dcdt(vector<dcomplex> state, const double omega, const double gamma) {
     dcomplex I(0, 1);
     vector<dcomplex> cc(pow(2,N));
     for(Size j=0; j<N; ++j) {
         for(Size i=0; i<pow(2, N); ++i) {
             Size jPrev = j == 0 ? N-1 : j-1;
             Size jNext = j == N-1 ? 0 : j+1;
-            Size jFlip = bitset<NN>(i).flip(j).to_ulong();
-            if(bitset<NN>(i)[j] == 1) {
+            Size jFlip = bitset<N>(i).flip(j).to_ulong();
+            if(bitset<N>(i)[j] == 1) {
                 cc[i] -= (dcomplex)(gamma/2.)*state[i]; // decay
             }
-            cc[jFlip] -= I*(dcomplex)(omega*(bitset<NN>(i)[jPrev]+bitset<NN>(i)[jNext]))*(state[i]); // Quantum part
+            cc[jFlip] -= I*(dcomplex)(omega*(bitset<N>(i)[jPrev]+bitset<N>(i)[jNext]))*(state[i]); // Quantum part
         }
     }
     return cc;
@@ -140,16 +139,15 @@ vector<dcomplex> QCP_1D::dcdt(vector<dcomplex> state, const Size N, const double
 
 int main(int argc, char *argv[]) {
     const Size   number_of_time_step = stoul(argv[1]);
-    const Size   N = stoul(argv[2]);
-    const double omega = stod(argv[3]);
-    const Size   number_of_ensemble = stoul(argv[4]);
+    const double omega = stod(argv[2]);
+    const Size   number_of_ensemble = stoul(argv[3]);
     const double gamma = 1;
     const double dt = 0.01;
 
     std::ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    QCP_1D* Model = new QCP_1D(number_of_time_step, N, omega, number_of_ensemble, gamma, dt);
+    QCP_1D* Model = new QCP_1D(number_of_time_step, omega, number_of_ensemble, gamma, dt);
     
     Model -> QJMC();
     delete Model;
